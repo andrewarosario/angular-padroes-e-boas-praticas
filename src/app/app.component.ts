55e8a685fae286be1cf0e528033cb0e8dc4927bf
andrew.rosario <andrew.arosario@gmail.com>
2021-02-08 08:26:50 -0300

    feat: incluindo título da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-padroes-e-boas-praticas';
+  title = 'TODO APP';
 }
