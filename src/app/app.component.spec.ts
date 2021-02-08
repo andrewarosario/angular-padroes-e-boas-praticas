3109c9229f2413fb0b739c6270fb3ba16f5d17dc
andrew.rosario <andrew.arosario@gmail.com>
2021-02-08 08:27:03 -0300

    test: incluindo título da aplicação

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppComponent ],
+      imports: [ RouterTestingModule ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have as title 'TODO APP'`, () => {
+    expect(component.title).toEqual('TODO APP');
+  });
+
+  it('should render title in a h1 tag', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent)
+      .toContain('TODO APP');
+  });
+});
