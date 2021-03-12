452f20d276db7be73b889442856a5165e129dbbc
Andrew Rosário <andrew.arosario@gmail.com>
2021-03-12 08:34:46 -0300

    feat: injetando StorageService no CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { LocalStorageService } from '../shared/storage/local-storage.service';
+import { StorageService } from './classes/storage.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule
+  ],
+  providers: [
+    LocalStorageService,
+    {
+      provide: StorageService,
+      useClass: LocalStorageService
+    }
   ]
 })
 export class CoreModule {
