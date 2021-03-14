7f372d037ab95d1c4ef0d063470fe5a8da10340c
Andrew Rosário <andrew.arosario@gmail.com>
2021-03-13 23:47:15 -0300

    feat: injetando dependências no TodosModule

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -5,17 +5,26 @@ import { TodosRoutingModule } from './todos-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TodoAddFormComponent } from './components/todo-add-form/todo-add-form.component';
+import { SEARCH_CONFIG_TODO } from './models/search-config-todo.model';
+import { TodosFacade } from './todos.facade';
+import { SearchConfigModule } from '../search-config/search-config.module';
+import { TodoSearchFormComponent } from './components/todo-search-form/todo-search-form.component';
 
 @NgModule({
   declarations: [
     TodoListComponent,
     TodoComponent,
-    TodoAddFormComponent
+    TodoAddFormComponent,
+    TodoSearchFormComponent
   ],
   imports: [
     SharedModule,
+    SearchConfigModule.forRoot(SEARCH_CONFIG_TODO),
     ReactiveFormsModule,
-    TodosRoutingModule
+    TodosRoutingModule,
+  ],
+  providers: [
+    TodosFacade
   ]
 })
 export class TodosModule { }
