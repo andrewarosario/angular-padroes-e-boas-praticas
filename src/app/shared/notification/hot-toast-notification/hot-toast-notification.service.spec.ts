65d351dd6632dce2b000bd82e716e965e1c820e9
Andrew Rosário <andrew.arosario@gmail.com>
2021-03-22 15:43:13 -0300

    test: testes do HotToastNotificationService

--- src/app/shared/notification/hot-toast-notification/hot-toast-notification.service.spec.ts
+++ src/app/shared/notification/hot-toast-notification/hot-toast-notification.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
+import { HotToastService } from '@ngneat/hot-toast';
 
 import { HotToastNotificationService } from './hot-toast-notification.service';
 
 describe('HotToastNotificationService', () => {
   let service: HotToastNotificationService;
+  let hotToastService: HotToastService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        HotToastService
+      ]
+    });
     service = TestBed.inject(HotToastNotificationService);
+    hotToastService = TestBed.inject(HotToastService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call success with correct value', () => {
+    const successSpy = spyOn(hotToastService, 'success');
+    service.success('Great!');
+    expect(successSpy).toHaveBeenCalledWith('Great!');
+  });
+
+  it('should call error with correct value', () => {
+    const errorSpy = spyOn(hotToastService, 'error');
+    service.error('Fail!');
+    expect(errorSpy).toHaveBeenCalledWith('Fail!');
   });
 });
